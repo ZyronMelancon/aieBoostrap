585b366842c2ea848864a2362292e2a3ababd7b0
ZyronMelancon <altausolaris@gmail.com>
2016-12-07 13:23:27 -0600

    Init Commit

--- project2D/Application2D.cpp
+++ project2D/Application2D.cpp
@@ -3,6 +3,16 @@
 #include "Font.h"
 #include "Input.h"
 
+
+struct ShipControl
+{
+	int x = 500;
+	int y = 500;
+	float r = 0;
+};
+
+ShipControl ship;
+
 Application2D::Application2D() {
 
 }
@@ -14,12 +24,7 @@ Application2D::~Application2D() {
 bool Application2D::startup() {
 	
 	m_2dRenderer = new aie::Renderer2D();
-
-	m_texture = new aie::Texture("./textures/numbered_grid.tga");
 	m_shipTexture = new aie::Texture("./textures/ship.png");
-
-	m_font = new aie::Font("./font/consolas.ttf", 32);
-
 	m_audio = new aie::Audio("./audio/powerup.wav");
 
 	m_cameraX = 0;
@@ -32,8 +37,6 @@ bool Application2D::startup() {
 void Application2D::shutdown() {
 	
 	delete m_audio;
-	delete m_font;
-	delete m_texture;
 	delete m_shipTexture;
 	delete m_2dRenderer;
 }
@@ -46,21 +49,28 @@ void Application2D::update(float deltaTime) {
 	aie::Input* input = aie::Input::getInstance();
 
 	// use arrow keys to move camera
-	if (input->isKeyDown(aie::INPUT_KEY_UP))
-		m_cameraY += 500.0f * deltaTime;
+	if (input->isKeyDown(aie::INPUT_KEY_UP) && !input->isKeyDown(aie::INPUT_KEY_DOWN))
+	{
+		ship.y += cos(ship.r) * 500.0f * deltaTime;
+		ship.x -= sin(ship.r) * 500.0f * deltaTime;
+	}
 
-	if (input->isKeyDown(aie::INPUT_KEY_DOWN))
-		m_cameraY -= 500.0f * deltaTime;
+	else if (input->isKeyDown(aie::INPUT_KEY_DOWN) && !input->isKeyDown(aie::INPUT_KEY_UP))
+	{
+		ship.y -= cos(ship.r) * 500.0f * deltaTime;
+		ship.x += sin(ship.r) * 500.0f * deltaTime;
+	}
+
+	else if (input->isKeyDown(aie::INPUT_KEY_UP) && input->isKeyDown(aie::INPUT_KEY_DOWN))
+	{
+		
+	}
 
 	if (input->isKeyDown(aie::INPUT_KEY_LEFT))
-		m_cameraX -= 500.0f * deltaTime;
+		ship.r += 0.12f;
 
 	if (input->isKeyDown(aie::INPUT_KEY_RIGHT))
-		m_cameraX += 500.0f * deltaTime;
-
-	// example of audio
-	if (input->wasKeyPressed(aie::INPUT_KEY_SPACE))
-		m_audio->play();
+		ship.r -= 0.12f;
 
 	// exit the application
 	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
@@ -78,34 +88,9 @@ void Application2D::draw() {
 	// begin drawing sprites
 	m_2dRenderer->begin();
 
-	// demonstrate animation
-	m_2dRenderer->setUVRect(int(m_timer) % 8 / 8.0f, 0, 1.f / 8, 1.f / 8);
-	m_2dRenderer->drawSprite(m_texture, 200, 200, 100, 100);
-
 	// demonstrate spinning sprite
 	m_2dRenderer->setUVRect(0,0,1,1);
-	m_2dRenderer->drawSprite(m_shipTexture, 600, 400, 0, 0, m_timer, 1);
-
-	// draw a thin line
-	m_2dRenderer->drawLine(300, 300, 600, 400, 2, 1);
-
-	// draw a moving purple circle
-	m_2dRenderer->setRenderColour(1, 0, 1, 1);
-	m_2dRenderer->drawCircle(sin(m_timer) * 100 + 600, 150, 50);
-
-	// draw a rotating red box
-	m_2dRenderer->setRenderColour(1, 0, 0, 1);
-	m_2dRenderer->drawBox(600, 500, 60, 20, m_timer);
-
-	// draw a slightly rotated sprite with no texture, coloured yellow
-	m_2dRenderer->setRenderColour(1, 1, 0, 1);
-	m_2dRenderer->drawSprite(nullptr, 400, 400, 50, 50, 3.14159f * 0.25f, 1);
-	
-	// output some text, uses the last used colour
-	char fps[32];
-	sprintf_s(fps, 32, "FPS: %i", getFPS());
-	m_2dRenderer->drawText(m_font, fps, 0, 720 - 32);
-	m_2dRenderer->drawText(m_font, "Press Space for sound!", 0, 720 - 64);
+	m_2dRenderer->drawSprite(m_shipTexture, ship.x, ship.y, 80, 80, ship.r, 2);
 
 	// done drawing sprites
 	m_2dRenderer->end();
